4aa3b4f45b82d71b70653988b365d1df25981ca4
Emil Privér <emil@rivercode.se>
2021-12-18 18:42:47 +0100

    dev

--- go.mod
+++ go.mod
@@ -1,5 +1,5 @@
 module github.com/emilpriver/serverless-function-go-locale-weather
 
-go 1.15
+go 1.16
 
 require github.com/aws/aws-lambda-go v1.27.1
--- main.go
+++ main.go
@@ -1,24 +1,18 @@
 package main
 
 import (
-	"context"
-	"fmt"
-
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(ctx context.Context, request events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
-	fmt.Println("This message will show up in the CLI console.")
-
-	return &events.APIGatewayProxyResponse{
-		StatusCode:      200,
-		Headers:         map[string]string{"Content-Type": "text/plain"},
-		Body:            "Hello, world!",
-		IsBase64Encoded: false,
+func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Body:       "Hello AWS Lambda and Netlify",
 	}, nil
 }
 
 func main() {
+	// Make the handler available for Remote Procedure Call by AWS Lambda
 	lambda.Start(handler)
 }
